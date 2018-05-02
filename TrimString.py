6cdf3b57373ea4126f9a2f88c0f74b4854bf449a
Yutian Li <Yutian.Li@sandstone.com.au>
2018-05-02 12:08:56 +1000

    Trim a string

---
+++ TrimString.py
@@ -0,0 +1,19 @@
+def trim(s):
+    s = str.lstrip(s)
+    s = str.rstrip(s)
+    return s
+
+if trim('hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello') != 'hello':
+    print('测试失败!')
+elif trim('  hello  ') != 'hello':
+    print('测试失败!')
+elif trim('  hello  world  ') != 'hello  world':
+    print('测试失败!')
+elif trim('') != '':
+    print('测试失败!')
+elif trim('    ') != '':
+    print('测试失败!')
+else:
+    print('测试成功!')
