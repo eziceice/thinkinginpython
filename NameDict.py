1b70b37f0365443f6f4e509310bd5f11a0bbba6e
eziceice <liyutian6@gmail.com>
2018-05-13 22:18:51 +1000

    Adding dict sample code

---
+++ NameDict.py
@@ -0,0 +1,34 @@
+def testDict():
+    people = {
+        'Alice': {
+            'phone': '2341',
+            'addr': 'Foo drive 23'
+        },
+        'Beth': {
+            'phone': '9102',
+            'addr': 'Bar street 42'
+        },
+        'Cecil': {
+            'phone': '3158',
+            'addr': 'Baz avenue 90'
+        }
+    }
+
+    labels = {
+        'phone': 'phone number',
+        'addr': 'address'
+    }
+
+    name = raw_input('Name: ')
+
+    request = raw_input('Phone number (p) or address (a)? ')
+
+    if request == 'p':
+        key = 'phone'
+    if request == 'a':
+        key = 'addr'
+
+    if name in people:
+        print(name + ' ' + labels[key] + ' ' + people[name][key])
+
+testDict()
