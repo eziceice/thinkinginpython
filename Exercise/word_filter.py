7777dac88246fe314546b8327fb0045270abdef5
Yutian Li <yutian.li@sandstone.com.au>
2018-08-13 17:01:35 +1000

    Adding replacement for bad words

--- Exercise/word_filter.py
+++ Exercise/word_filter.py
@@ -10,10 +10,14 @@ if __name__ == '__main__':
     words = filter_word('resources/filtered_words.txt')
     while True:
         text = input('Please enter a word:')
-        if text in words:
-            print('Freedom')
-        elif text == 'exit':
-            print('Exit')
-            exit()
-        else:
-            print('Human Rights')
+        # if text in words:
+        #     print('Freedom')
+        # elif text == 'exit':
+        #     print('Exit')
+        #     exit()
+        # else:
+        #     print('Human Rights')
+        for word in words:
+            if word in text:
+                text = text.replace(word, len(word) * '*')
+        print(text)
